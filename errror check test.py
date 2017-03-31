dbe14eaf63f50b8e14717ba3fa916a24a99ae41e
AaRoN <aaron.hunt@bourne-grammar.lincs.sch.uk>
2017-03-31 21:48:02 +0100

    Update errror check test.py
    Added the option to test for other errors than just ValueError. For usage, type the error you want as the fourth item imported.

--- errror check test.py
+++ errror check test.py
@@ -1,4 +1,4 @@
-def errorCheck(varName, type, question):
+def errorCheck(varName, type, question, error):
     x=0
     while x == 0:
         try:
@@ -6,7 +6,7 @@ def errorCheck(varName, type, question):
             #tries to convert the variable into an integer/float
             x=1
             #stops the loop
-        except ValueError:
+        except error:
             varName = input(question)
             #if it is unable to convert the users input it asks the question again
     return varName
